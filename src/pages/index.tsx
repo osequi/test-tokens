03c84e8edc7552f4202720bc7609c7e06e1ed70c
osequi <cs@osequi.com>
2020-12-15 10:43:43 +0200

    All done. eady for the hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,22 @@ const Home = () => {
     (item) => item.id.type === "scale" && item.id.name === "modular"
   );
 
+  const defaultLink = tokens().find(
+    (item) => item.id.type === "link" && item.id.name === "default"
+  );
+
+  const defaultLinkDefaultState = defaultLink.styles.find(
+    (style) => style.state === "default"
+  );
+
   return (
     <>
       <ul>
         <li>Linear 10: {JSON.stringify(linear)}</li>
         <li>Modular 5: {JSON.stringify(modular)}</li>
+        <li>
+          Default link, default state: {JSON.stringify(defaultLinkDefaultState)}
+        </li>
       </ul>
       <hr />
       <ul>
